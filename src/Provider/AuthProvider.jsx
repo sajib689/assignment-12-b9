87f267f340305f75fecccecf25f311634fc71aab
sajib689 <jibonraja11@gmail.com>
2024-06-01 11:27:04 +0600

    add nav

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,55 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from './../Firebase/Firebase.config';
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth/web-extension";
+
+
+export const AuthContext = createContext(null)
+const gitHubProvider = new GithubAuthProvider()
+const googleProvider = new GoogleAuthProvider()
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const register = (email, password) => {
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth, email, password)
+   
+    }
+    const login = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const gitHub = () => {
+    setLoading(true)
+    return signInWithPopup(auth, gitHubProvider)
+    }
+    const google = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    useEffect(() => {
+       const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setLoading(false)
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe()
+        }
+    },[])
+    const authInfo = {
+        user,
+        loading,
+        register,
+        login,
+        gitHub,
+        google
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
