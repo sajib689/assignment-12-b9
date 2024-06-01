912d01a01a0a7132c98d3aa2e8f44e0504ffccd0
sajib689 <jibonraja11@gmail.com>
2024-06-01 12:39:44 +0600

    add google fonts

---
+++ src/Components/TopScholarships.jsx
@@ -0,0 +1,11 @@
+
+
+const TopScholarships = () => {
+    return (
+        <div className="mt-24 mb-24 max-w-6xl mx-auto">
+            
+        </div>
+    );
+};
+
+export default TopScholarships;
--- src/Outlet/Home.jsx
+++ src/Outlet/Home.jsx
@@ -1,10 +1,12 @@
 import BannerSlider from "../Components/BannerSlider";
+import TopScholarships from "../Components/TopScholarships";
 
 
 const Home = () => {
     return (
         <div>
             <BannerSlider/>
+            <TopScholarships/>
         </div>
     );
 };
