975551afc3f25350307d296a39112156da98dda9
sajib689 <jibonraja11@gmail.com>
2024-06-04 16:40:29 +0600

    map the application card

---
+++ src/Components/Users/UserApplicationCard.jsx
@@ -0,0 +1,33 @@
+
+
+const UserApplicationCard = () => {
+    return (
+        <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>123 University Ave, City, Country</td>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="font-bold">Good application</div>
+            <div className="text-sm opacity-50">No issues found</div>
+          </div>
+        </td>
+        <td>Computer Science</td>
+        <td>Bachelor of Science</td>
+        <td>$100</td>
+        <td>$20</td>
+        <td>
+          <span className="badge badge-info badge-sm">Pending</span>
+        </td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+      
+    );
+};
+
+export default UserApplicationCard;
--- src/Components/Users/UserApplications.jsx
+++ src/Components/Users/UserApplications.jsx
@@ -1,11 +1,75 @@
-
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../Hooks/useAuth";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import Loader from "../../Utilities/Loader";
+import UserApplicationCard from "./UserApplicationCard";
 
 const UserApplications = () => {
-    return (
-        <div>
-            
+  const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
+  const { data: applications = [], isPending } = useQuery({
+    queryKey: ["applications"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/applications?email=${user?.email}`);
+      return res.data;
+    },
+  });
+
+  if (isPending) return <Loader />;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mt-5">
+        All Applications: {applications.length}
+      </h1>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <th>University Address</th>
+                <th>Application Feedback</th>
+                <th>Subject Category</th>
+                <th>Applied Degree</th>
+                <th>Application Fees</th>
+                <th>Service Charge</th>
+                <th>Application Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {applications.map((application) => (
+                <UserApplicationCard
+                  key={application._id}
+                  application={application}
+                ></UserApplicationCard>
+              ))}
+            </tbody>
+            {/* foot */}
+            <tfoot>
+              <tr>
+                <th></th>
+                <th>University Address</th>
+                <th>Application Feedback</th>
+                <th>Subject Category</th>
+                <th>Applied Degree</th>
+                <th>Application Fees</th>
+                <th>Service Charge</th>
+                <th>Application Status</th>
+                <th></th>
+              </tr>
+            </tfoot>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserApplications;
