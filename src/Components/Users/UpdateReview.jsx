44e7b773c230b33d007aa9696e13683b32fa2bc4
sajib689 <jibonraja11@gmail.com>
2024-06-04 22:32:10 +0600

    update review success done

--- src/Components/Users/UpdateReview.jsx
+++ src/Components/Users/UpdateReview.jsx
@@ -1,14 +1,46 @@
+import { Rating } from "primereact/rating";
+import { useState } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 
 const UpdateReview = () => {
+    const review = useLoaderData()
+    const [value, setValue] = useState(review?.reviewer_rating)
+    const {_id,reviewer_comments} = review
+    const axiosPublic = useAxiosPublic()
+    const navigate = useNavigate()
+    const handleUpdateReview = e => {
+        e.preventDefault()
+        const form = e.target
+        const reviewer_comments = form.review.value
+        const reviewer_rating = value
+        const data = {
+            reviewer_comments,
+            reviewer_rating
+        }
+        axiosPublic.put(`/reviews/${review?._id}`,data)
+        .then(res => {
+            
+            if (res.data) {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your review has been deleted.",
+                  icon: "success",
+                });
+              }
+            navigate('/userDashboard/userReview')
+        })
+    }
     return (
-        <div className="flex flex-col w-[410px] md:w-full lg:w-full md:p-8  shadow-sm rounded-xl lg:p-12 dark:bg-gray-50 dark:text-gray-800">
+        <div className="flex flex-col justify-center items-center w-[410px] mx-auto md:w-[750px] lg:w-[750px] md:p-8  shadow-sm rounded-xl lg:p-12 dark:bg-gray-50 dark:text-gray-800">
         <form
-          onSubmit={handleSubmit}
-          className="flex flex-col items-center w-full"
+          onSubmit={handleUpdateReview}
+          className="flex flex-col items-center justify-center w-full"
         >
           <h2 className="text-3xl font-semibold text-center">
-            Your opinion matters!
+          Update Your opinion matters!
           </h2>
           <div className="flex flex-col items-center py-6 space-y-3">
             <span className="text-center">How was your experience?</span>
@@ -23,6 +55,7 @@ const UpdateReview = () => {
           </div>
           <div className="flex flex-col w-full">
             <textarea
+            defaultValue={reviewer_comments}
             required
               name="review"
               rows="3"
@@ -33,7 +66,7 @@ const UpdateReview = () => {
               type="submit"
               className="py-4 bg-blue-600 text-white my-8 font-semibold rounded-md dark:text-gray-50 dark:bg-blue-600"
             >
-              Leave feedback
+              Update feedback
             </button>
           </div>
         </form>
