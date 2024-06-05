8f211bee2de97f6b9876f0665ab00013c40401bc
sajib689 <jibonraja11@gmail.com>
2024-06-05 17:48:34 +0600

    role add success

--- src/Components/Admin/ManageUsersCard.jsx
+++ src/Components/Admin/ManageUsersCard.jsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
-import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { useState } from "react";
 import Swal from "sweetalert2";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const ManageUsersCard = ({ userAll, index, refetch }) => {
   const { _id, name, email, role, image } = userAll;
   const axiosSecure = useAxiosSecure();
+  const [selectedRole, setSelectedRole] = useState(role); 
+
+  // Handle delete user
   const handleDeleteUser = (_id) => {
     Swal.fire({
       title: "Are you sure?",
@@ -16,13 +19,12 @@ const ManageUsersCard = ({ userAll, index, refetch }) => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        axiosSecure.delete(`/users/${_id}`)
-        .then((res) => {
+        axiosSecure.delete(`/users/${_id}`).then((res) => {
           refetch();
           if (res.data) {
             Swal.fire({
               title: "Deleted!",
-              text: "Your Application has been deleted.",
+              text: "The user has been deleted.",
               icon: "success",
             });
           }
@@ -30,16 +32,46 @@ const ManageUsersCard = ({ userAll, index, refetch }) => {
       }
     });
   };
+
+  // Handle role change
+  const handleRoleChange = (e) => {
+    const newRole = e.target.value;
+    setSelectedRole(newRole); 
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: `Do you want to change the role to ${newRole}?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, change it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.patch(`/users/${_id}/role`, { role: newRole }).then((res) => {
+          refetch();
+          if (res.data) {
+            Swal.fire({
+              title: "Updated!",
+              text: `The user role has been changed to ${newRole}.`,
+              icon: "success",
+            });
+          }
+        });
+      } else {
+        setSelectedRole(role); 
+      }
+    });
+  };
+
   return (
     <tr>
       <th>
         <label>{index + 1}</label>
       </th>
       <td>{name}</td>
-
       <td>{email}</td>
       <td>{role}</td>
-
       <td>
         <button
           onClick={() => handleDeleteUser(_id)}
@@ -49,12 +81,15 @@ const ManageUsersCard = ({ userAll, index, refetch }) => {
         </button>
       </td>
       <td>
-        <select className="select select-bordered w-xl max-w-xs">
-          <option disabled selected>
-            Select Role
-          </option>
-          <option value='moderator'>Moderator</option>
-          <option value='moderator'>Admin</option>
+        <select
+          name="role"
+          className="select select-bordered w-xl max-w-xs"
+          value={selectedRole} 
+          onChange={handleRoleChange} 
+        >
+          <option disabled>Select Role</option>
+          <option value="moderator">Moderator</option>
+          <option value="admin">Admin</option>
         </select>
       </td>
     </tr>
