a996fbeab59369dfe346e16599a5cb5ee02a2064
sajib689 <jibonraja11@gmail.com>
2024-06-06 11:44:20 +0600

    try to implement to aproved application status

--- src/Components/Users/UserApplicationCard.jsx
+++ src/Components/Users/UserApplicationCard.jsx
@@ -1,10 +1,20 @@
+import Swal from "sweetalert2";
+import useAxiosPublic from "./../../Hooks/useAxiosPublic";
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import useAuth from "../../Hooks/useAuth";
 
-import Swal from 'sweetalert2';
-import useAxiosPublic from './../../Hooks/useAxiosPublic';
-import { Link } from 'react-router-dom';
-
-const UserApplicationCard = ({ application, index,refetch }) => {
-
+const UserApplicationCard = ({ application, index, refetch }) => {
+  const axiosSecure = useAxiosSecure()
+  const {user} = useAuth()
+  const {data: role = []} = useQuery({
+    queryKey: ['role',user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/role/${user?.email}`)
+      return res.data
+    }
+  })
   const {
     _id,
     university_name,
@@ -14,8 +24,8 @@ const UserApplicationCard = ({ application, index,refetch }) => {
     application_fees,
     service_charge,
   } = application;
-  const axiosPublic = useAxiosPublic()
-  const handleDeleteApplication = _id => {
+  const axiosPublic = useAxiosPublic();
+  const handleDeleteApplication = (_id) => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -23,48 +33,65 @@ const UserApplicationCard = ({ application, index,refetch }) => {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!"
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        axiosPublic.delete(`/applications/${_id}`)
-        .then(res => {
-          refetch()
-         if(res.data){
-          Swal.fire({
-            title: "Deleted!",
-            text: "Your Application has been deleted.",
-            icon: "success"
-          });
-         }
-        })
-       
+        axiosPublic.delete(`/applications/${_id}`).then((res) => {
+          refetch();
+          if (res.data) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your Application has been deleted.",
+              icon: "success",
+            });
+          }
+        });
       }
     });
-   
-  }
+  };
+  console.log(role.role)
   return (
     <tr>
       <th>
         <label>{index + 1}</label>
       </th>
       <td>{university_name}</td>
-      
+
       <td>{university_address}</td>
-      <td>
-       Good Application
-      </td>
+      <td>Good Application</td>
       <td>{subject_category}</td>
       <td>{applied_degree}</td>
       <td>${application_fees}</td>
       <td>${service_charge}</td>
       <td>
-        <span className="badge badge-info badge-sm text-white">processing</span>
+        {
+          role.role === 'user' &&
+          <button className="badge badge-info badge-sm text-white">
+          processing
+        </button>
+        }
+        {
+          role.role === 'admin' &&
+          <button className="badge badge-info badge-sm text-white">
+          pending
+        </button>
+        }
       </td>
       <th>
-        <button onClick={() => handleDeleteApplication(_id)} className="btn btn-warning btn-xs text-white">Delete</button>
+        <button
+          onClick={() => handleDeleteApplication(_id)}
+          className="btn btn-warning btn-xs text-white"
+        >
+          Delete
+        </button>
       </th>
       <th>
-        <Link to={`/userDashboard/updateapplication/${_id}`} className="btn btn-warning btn-xs text-white">Update</Link>
+        <Link
+          to={`/userDashboard/updateapplication/${_id}`}
+          className="btn btn-warning btn-xs text-white"
+        >
+          Update
+        </Link>
       </th>
     </tr>
   );
