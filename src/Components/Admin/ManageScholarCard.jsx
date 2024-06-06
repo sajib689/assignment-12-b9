9088670976c4e95cad2836187b7d4dcaa0e0c511
sajib689 <jibonraja11@gmail.com>
2024-06-06 17:49:13 +0600

    modal update

--- src/Components/Admin/ManageScholarCard.jsx
+++ src/Components/Admin/ManageScholarCard.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { MdDelete, MdOutlinePreview } from "react-icons/md";
+import { useState } from "react";
 
 const ManageScholarCard = ({ scholar, refetch, index }) => {
   const {
@@ -12,8 +13,18 @@ const ManageScholarCard = ({ scholar, refetch, index }) => {
     applicationFees,
     universityImage
   } = scholar;
-  console.log(scholar);
+  
+  const [details, setDetails] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const axiosSecure = useAxiosSecure();
+
+  const handleDetails = async (_id) => {
+    const res = await axiosSecure.get(`/university/${_id}`);
+    setDetails(res.data);
+    setIsModalOpen(true);
+  };
+
   const handleDeleteScholar = (_id) => {
     Swal.fire({
       title: "Are you sure?",
@@ -38,51 +49,60 @@ const ManageScholarCard = ({ scholar, refetch, index }) => {
       }
     });
   };
+
   return (
-    <tr>
-      <th>
-        <label>{index + 1}</label>
-      </th>
-      <td>{scholarshipCategory}</td>
-      <td>{universityName}</td>
-      <td>{subjectName}</td>
-      <td>{}</td>
-      <td>${applicationFees}</td>
-      <th>
-        <button
-          className="btn"
-          onClick={() => document.getElementById("my_modal_2").showModal()}
-        >
-          <MdOutlinePreview className="text-2xl text-green-700" />
-        </button>
-        <dialog id="my_modal_2" className="modal">
-          <div className="modal-box flex flex-col justify-center items-center">
-            <img className="mt-5" src={universityImage} alt="" />
-            <h3 className="font-bold text-lg">{universityName}</h3>
-            <p className="py-4">Press ESC key or click outside to close</p>
+    <>
+      <tr>
+        <th>
+          <label>{index + 1}</label>
+        </th>
+        <td>{scholarshipCategory}</td>
+        <td>{universityName}</td>
+        <td>{subjectName}</td>
+        <td>{}</td>
+        <td>${applicationFees}</td>
+        <th>
+          <button
+            className="btn"
+            onClick={() => handleDetails(_id)}
+          >
+            <MdOutlinePreview className="text-2xl text-green-700" />
+          </button>
+        </th>
+        <th>
+          <button
+            onClick={() => handleDeleteScholar(_id)}
+            className="btn btn-warning btn-xs text-white"
+          >
+            <MdDelete className="text-2xl text-red-500" />
+          </button>
+        </th>
+        <th>
+          <Link
+            to={`/userDashboard/updateapplication/${_id}`}
+            className="btn btn-warning btn-xs text-white"
+          >
+            Update
+          </Link>
+        </th>
+      </tr>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="modal-box bg-white p-6 rounded shadow-lg">
+            <div className="flex flex-col justify-center items-center">
+              <img className="mt-5" src={details.universityImage} alt="University" />
+              <h3 className="font-bold text-lg mt-4">{details.universityName}</h3>
+              <p className="py-4">{details.scholarshipDescription}</p>
+              <button className="btn" onClick={() => setIsModalOpen(false)}>
+                Close
+              </button>
+            </div>
           </div>
-          <form method="dialog" className="modal-backdrop">
-            <button>close</button>
-          </form>
-        </dialog>
-      </th>
-      <th>
-        <button
-          onClick={() => handleDeleteScholar(_id)}
-          className="btn btn-warning btn-xs text-white"
-        >
-          <MdDelete className="text-2xl text-red-500" />
-        </button>
-      </th>
-      <th>
-        <Link
-          to={`/userDashboard/updateapplication/${_id}`}
-          className="btn btn-warning btn-xs text-white"
-        >
-          Update
-        </Link>
-      </th>
-    </tr>
+          <div className="modal-backdrop fixed inset-0 bg-black opacity-50"></div>
+        </div>
+      )}
+    </>
   );
 };
 
